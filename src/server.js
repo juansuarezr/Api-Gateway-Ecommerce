2a41b4ea03483dcdc9510fa56a912d7a50285e6b
juansuarezr <juan.suarezr@udea.edu.co>
2021-08-22 11:47:36 -0500

    Estructura base

---
+++ src/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    users_api_url: process.env.USERS_API_URL || 'https://mision-tic-users-jpsr.herokuapp.com',
+    account_api_url: process.env.ACCOUNT_API_URL || 'https://cuentasmsjpsr.herokuapp.com',
+    
+};
